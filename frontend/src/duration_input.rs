2596a3fa24b9d4137e80c1ae73838f7008e2f1bd
zUnixorn <dev@jonascrull.de>
2023-10-05 03:47:42 +0200

    made duration input much better

--- frontend/src/duration_input.rs
+++ frontend/src/duration_input.rs
@@ -1,126 +1,40 @@
 use std::fmt::{Display, Formatter};
 use std::mem;
-use gloo_timers::callback::Timeout;
 
 use strum::FromRepr;
-use tracing::debug;
-use web_sys::{Event, FocusEvent, HtmlInputElement, InputEvent, KeyboardEvent, MouseEvent};
+use web_sys::{DragEvent, Event, FocusEvent, HtmlInputElement, KeyboardEvent, MouseEvent};
 use yew::{AttrValue, Callback, Component, Context, Html, html, NodeRef, Properties};
 
 pub struct DurationInput {
     duration: Duration,
-}
-
-struct Duration {
-    hours: u32,
-    minutes: u32,
-    seconds: u32,
-    selection: Selection,
-    sub_cursor: bool,
-}
-
-impl Duration {
-    const MAX: u32 = 99 * 60 * 60 + 59 * 60 + 59;
-
-    fn add_selected(&mut self, n: i32) -> bool {
-        let secs = (self.hours * 60 * 60 + self.minutes * 60 + self.seconds) as i32;
-        let add = self.selection.to_seconds() as i32 * n;
-        let sum = secs + add;
-
-        if sum.is_negative() || sum as u32 > Self::MAX || (self.get_selection_field(self.selection).unwrap() as i32 + n).is_negative() {
-            return false;
-        }
-
-        self.add_to(self.selection, n);
-
-        true
-    }
-
-    fn add_to(&mut self, to: Selection, n: i32) {
-        let sum = self.get_selection_field(to).unwrap() as i32 + n;
-        let reminder = sum % (to.max() + 1);
-        let next = sum / (to.max() + 1);
-
-        self.set_selection(to, reminder as u32);
-
-        if sum > to.max() {
-            self.add_to(to.left(), next);
-        };
-    }
-
-    fn get_selection_field(&self, s: Selection) -> Option<u32> {
-        match s {
-            Selection::Hour => Some(self.hours),
-            Selection::Minute => Some(self.minutes),
-            Selection::Second => Some(self.seconds),
-            Selection::Empty => None,
-        }
-    }
-
-    fn set_selection(&mut self, s: Selection, n: u32) {
-        match s {
-            Selection::Hour => self.hours = n,
-            Selection::Minute => self.minutes = n,
-            Selection::Second => self.seconds = n,
-            _ => (),
-        }
-    }
-
-    fn update_selection(&mut self, s: Selection) {
-        if self.selection != s {
-            self.selection = s;
-            self.sub_cursor = false;
-        }
-    }
-
-    fn get_selection(&self) -> Selection {
-        self.selection
-    }
-
-    fn type_digit(&mut self, n: u32) {
-        let number = self.get_selection_field(self.selection).unwrap();
-
-        // if self.sub_cursor {
-        //     number += n;
-        // } else {
-        //     number += n * 10;
-        // }
-
-        self.sub_cursor != self.sub_cursor;
-    }
+    selection: Option<Selection>,
+    backspace: bool,
 }
 
 #[derive(FromRepr, Copy, Clone, PartialEq)]
 enum Selection {
-    Hour = 0,
-    Minute = 1,
-    Second = 2,
-    Empty = 4,
+    Hours = 0,
+    Minutes = 1,
+    Seconds = 2,
 }
 
 impl Selection {
-    fn from_cursor_position(cursor: u32) -> Self {
+    fn select(&self, input: &HtmlInputElement) {
+        let start = (*self as usize * 3) as u32;
+        input.set_selection_start(Some(start)).unwrap();
+        input.set_selection_end(Some(start + 2)).unwrap();
+    }
+
+    fn from_cursor(cursor: u32) -> Self {
         if cursor > 8 {
-            panic!()
+            panic!();
         }
 
-        Self::from_repr((cursor / 3) as usize).unwrap()
-    }
-
-    fn selection_start(&self) -> u32 {
-        if *self == Self::Empty {
-            panic!()
-        }
-
-        (*self as usize * 3) as u32
-    }
-
-    fn selection_end(&self) -> u32 {
-        self.selection_start() + 2
+        Self::from_repr(cursor as usize / 3).unwrap()
     }
 
     fn left(&self) -> Self {
-        if *self == Self::Hour {
+        if *self == Self::Hours {
             return *self;
         }
 
@@ -128,34 +42,111 @@ impl Selection {
     }
 
     fn right(&self) -> Self {
-        if *self == Self::Second {
+        if *self == Self::Seconds {
             return *self;
         }
 
         Self::from_repr(*self as usize + 1).unwrap()
     }
 
-    fn max(&self) -> i32 {
+    fn part_with(&self, n: i32) -> Parts {
+        let mut parts = Parts {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        };
+        
         match self {
-            Selection::Hour => 99,
-            Selection::Minute | Selection::Second => 59,
-            _ => panic!()
+            Selection::Hours => parts.hours = n,
+            Selection::Minutes => parts.minutes = n,
+            Selection::Seconds => parts.seconds = n,
+        }
+
+        parts
+    }
+}
+
+struct Duration {
+    seconds: u32,
+    sub_cursor: bool,
+}
+
+#[derive(Copy, Clone)]
+struct Parts {
+    hours: i32,
+    minutes: i32,
+    seconds: i32,
+}
+
+impl Parts {
+    const SECONDS_HOUR: i32 = 60 * 60;
+    const SECONDS_MINUTES: i32 = 60;
+
+    fn zero_selection(&mut self, selection: Selection) {
+        match selection {
+            Selection::Hours => self.hours = 0,
+            Selection::Minutes => self.minutes = 0,
+            Selection::Seconds => self.seconds = 0,
         }
     }
 
     fn to_seconds(&self) -> u32 {
-        match self {
-            Selection::Hour => 60 * 60,
-            Selection::Minute => 60,
-            Selection::Second => 1,
-            Selection::Empty => 0
+        (self.hours * Self::SECONDS_HOUR + self.minutes * Self::SECONDS_MINUTES + self.seconds) as u32
+    }
+
+    fn valid(&self) -> bool {
+        self.seconds < 60 && self.minutes < 60 && self.hours < 100
+    }
+}
+
+impl Duration {
+    const MAX_SECONDS: i32 = 99 * 60 * 60 + 59 * 60 + 59;
+
+    fn to_parts(&self) -> Parts {
+        let hours = self.seconds as i32 / Parts::SECONDS_HOUR;
+        let reminder = self.seconds as i32 % Parts::SECONDS_HOUR;
+        let minutes = reminder / Parts::SECONDS_MINUTES;
+        let seconds = reminder % Parts::SECONDS_MINUTES;
+
+        Parts {
+            hours,
+            minutes,
+            seconds,
         }
+    }
+
+    fn from_parts(parts: Parts) -> Self {
+        Self {
+            seconds: parts.to_seconds(),
+            sub_cursor: false,
+        }
+    }
+
+    fn add_parts(&mut self, parts: Parts) {
+        self.sub_cursor = false;
+
+        let seconds = self.seconds as i32 + parts.seconds +
+            parts.minutes * Parts::SECONDS_MINUTES +
+            parts.hours * Parts::SECONDS_HOUR;
+
+        if seconds > Self::MAX_SECONDS || seconds < 0 {
+            return;
+        }
+
+        self.seconds = seconds as u32;
+    }
+
+    fn reset(&mut self) {
+        self.seconds = 0;
+        self.sub_cursor = false;
     }
 }
 
 impl Display for Duration {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{:02}:{:02}:{:02}", self.hours, self.minutes, self.seconds))
+        let parts = self.to_parts();
+
+        write!(f, "{:02}:{:02}:{:02}", parts.hours, parts.minutes, parts.seconds)
     }
 }
 
@@ -165,33 +156,50 @@ pub struct DurationInputProps {
     pub input_ref: NodeRef,
 }
 
-fn unselect(input: HtmlInputElement) -> u32 {
+fn cursor_location(input: &HtmlInputElement) -> u32 {
     let direction = input.selection_direction().unwrap().unwrap();
 
     let mut start = input.selection_start().unwrap().unwrap();
     let mut end = input.selection_end().unwrap().unwrap();
 
     if direction == "forward" {
-        input.set_selection_end(Some(start)).unwrap();
         mem::swap(&mut start, &mut end);
-    } else {
-        input.set_selection_start(Some(end)).unwrap();
-    };
+    }
 
     start
 }
 
+// fn unselect(input: &HtmlInputElement) -> u32 {
+//     let direction = input.selection_direction().unwrap().unwrap();
+//
+//     let mut start = input.selection_start().unwrap().unwrap();
+//     let mut end = input.selection_end().unwrap().unwrap();
+//
+//     if direction == "forward" {
+//         input.set_selection_end(Some(start)).unwrap();
+//         mem::swap(&mut start, &mut end);
+//     } else {
+//         input.set_selection_start(Some(end)).unwrap();
+//     };
+//
+//     start
+// }
+
+#[derive(PartialEq)]
 pub enum DurationInputMessage {
-    Select(Selection),
     Key(Key),
+    MouseUp,
+    Focus(bool)
 }
 
+#[derive(PartialEq)]
 enum Key {
     Right,
     Left,
     Up,
     Down,
     Digit(i32),
+    Backspace,
 }
 
 impl Component for DurationInput {
@@ -201,74 +209,117 @@ impl Component for DurationInput {
     fn create(_: &Context<Self>) -> Self {
         Self {
             duration: Duration {
-                hours: 0,
-                minutes: 0,
                 seconds: 0,
-                selection: Selection::Empty,
                 sub_cursor: false
-            }
+            },
+            selection: None,
+            backspace: false,
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         let input = ctx.props().input_ref.cast::<HtmlInputElement>().unwrap();
 
-        // TODO remove this and figure out a better way, but this works for now
-        fn remove_me(a: &DurationInput, ctx: &Context<DurationInput>) {
-            let s = a.duration.get_selection();
-            let c = ctx.link().callback(move |_| {
-                DurationInputMessage::Select(s)
-            });
-            let t = Timeout::new(1, move || {
-                c.emit(());
-            });
-            t.forget();
+        if msg != DurationInputMessage::Key(Key::Backspace) {
+            self.backspace = false;
         }
 
         match msg {
-            DurationInputMessage::Select(s) => {
-                self.duration.selection = s;
-            }
-            DurationInputMessage::Key(key) => match key {
-                Key::Right => self.duration.update_selection(self.duration.selection.right()),
-                Key::Left => self.duration.update_selection(self.duration.selection.left()),
+            DurationInputMessage::MouseUp => {
+                let cursor = cursor_location(&input);
+                let selection = Selection::from_cursor(cursor);
+                self.selection = Some(selection);
+                self.duration.sub_cursor = false;
+            },
+            DurationInputMessage::Key(key) if self.selection.is_some() => match key {
+                Key::Right => {
+                    self.selection = Some(self.selection.unwrap().right());
+                    self.selection.unwrap().select(&input);
+                },
+                Key::Left => {
+                    self.selection = Some(self.selection.unwrap().left());
+                    self.selection.unwrap().select(&input);
+                },
                 Key::Up => {
-                    self.duration.add_selected(1);
-                    remove_me(&self, ctx);
+                    let parts = self.selection.unwrap().part_with(1);
+                    self.duration.add_parts(parts);
                 },
                 Key::Down => {
-                    self.duration.add_selected(-1);
-                    remove_me(&self, ctx);
+                    let parts = self.selection.unwrap().part_with(-1);
+                    self.duration.add_parts(parts);
                 },
-                Key::Digit(n) => debug!("Pressed: {}", n),
+                Key::Digit(mut n) => {
+                    let sub_cursor = self.duration.sub_cursor;
+
+                    if !sub_cursor {
+                        n *= 10;
+                    }
+
+                    let parts = self.selection.unwrap().part_with(n);
+
+                    if parts.valid() {
+                        if !sub_cursor {
+                            let mut zero_parts = self.duration.to_parts();
+                            zero_parts.zero_selection(self.selection.unwrap());
+                            self.duration.seconds = zero_parts.to_seconds();
+                        }
+
+                        self.duration.add_parts(parts);
+
+                        if sub_cursor {
+                            self.selection = Some(self.selection.unwrap().right());
+                        } else {
+                            self.duration.sub_cursor = true;
+                        }
+                    }
+                },
+                Key::Backspace => {
+                    self.duration.sub_cursor = false;
+
+                    if self.backspace {
+                        self.backspace = false;
+
+                        self.duration.reset();
+                    } else {
+                        self.backspace = true;
+
+                        let mut parts = self.duration.to_parts();
+                        parts.zero_selection(self.selection.unwrap());
+                        self.duration.seconds = parts.to_seconds();
+                    }
+                },
+            },
+            DurationInputMessage::Focus(focus) => {
+                if focus {
+                    if self.selection.is_none() {
+                        let cursor = cursor_location(&input);
+                        let selection = Selection::from_cursor(cursor);
+                        self.selection = Some(selection);
+                    }
+                } else {
+                    self.selection = None;
+                }
             },
             _ => (),
         }
-
-        input.set_selection_start(Some(self.duration.get_selection().selection_start())).unwrap();
-        input.set_selection_end(Some(self.duration.get_selection().selection_end())).unwrap();
 
         true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         /*
-            inputBox.addEventListener('keydown', handleKeydown);
-            // selects a block of hours, minutes etc (useful when focused by keyboard: Tab)
-            inputBox.addEventListener('focus', handleInputFocus);
-            // selects a block of hours, minutes etc (useful when clicked on PC or tapped on mobile)
-            inputBox.addEventListener('mouseup', handleClickFocus);
-            inputBox.addEventListener('change', insertAndApplyValidations);
-            // prefer 'input' event over 'keyup' for soft keyboards on mobile
-            inputBox.addEventListener('input', handleUserInput);
-            inputBox.addEventListener('blur', handleInputBlur);
-            inputBox.addEventListener('drop', cancelDefaultEvent);
+            TODO
+                - make tab focus better
+                - maybe find a way to overwrite onmousedown
+                - prevent dragging stuff out
+                - add up/down buttons
         */
 
         let send_key = ctx.link().callback(|key| {
             DurationInputMessage::Key(key)
         });
 
+        // TODO allow numpad and other numbers
         let onkeydown = Callback::from(move |event: KeyboardEvent| {
             match event.code().as_str() {
                 "ArrowLeft" => send_key.emit(Key::Left),
@@ -279,35 +330,59 @@ impl Component for DurationInput {
                     let n = d.chars().last().unwrap().to_digit(10).unwrap();
                     send_key.emit(Key::Digit(n as i32));
                 },
+                "Backspace" => send_key.emit(Key::Backspace),
                 _ => (),
             }
 
             event.prevent_default();
         });
 
-        let select = ctx.link().callback(|selection| {
-            DurationInputMessage::Select(selection)
-        });
-
         let input_ref = ctx.props().input_ref.clone();
-        let onmouseup = Callback::from(move |event: MouseEvent| {
-            let input = input_ref.cast::<HtmlInputElement>().unwrap();
-            let cursor = unselect(input);
-
-            select.emit(Selection::from_cursor_position(cursor));
-
+        let onmouseup = ctx.link().callback(|event: MouseEvent| {
             event.prevent_default();
+            DurationInputMessage::MouseUp
         });
 
         let onpaste = Callback::from(|event: Event| {
             event.prevent_default();
         });
 
+        let onfocus = ctx.link().callback(|event: FocusEvent| {
+            DurationInputMessage::Focus(true)
+        });
+
+        let onblur = ctx.link().callback(|event: FocusEvent| {
+            DurationInputMessage::Focus(false)
+        });
+
+        let ondrop = Callback::from(|event: DragEvent| {
+            event.prevent_default();
+        });
+
         html! {
             <>
-                <input onpaste={ onpaste } onkeydown={ onkeydown } onmouseup={ onmouseup } ref={ ctx.props().input_ref.clone() } name={ ctx.props().name.clone() } pattern="^[0-9]{1,2}:[0-5][0-9]:[0-5][0-9]$" style="text-align: right;" type="text" value={ format!("{}", self.duration) }/>
+                <input
+                    { onblur }
+                    { onfocus }
+                    { ondrop }
+                    { onpaste }
+                    { onkeydown }
+                    { onmouseup }
+                    ref={ ctx.props().input_ref.clone() }
+                    name={ ctx.props().name.clone() }
+                    pattern="^[0-9]{1,2}:[0-5][0-9]:[0-5][0-9]$"
+                    style="text-align: right;"
+                    type="text"
+                    value={ format!("{}", self.duration) }/>
             </>
         }
-        // pattern=r#"^(?:\d{1,2}:){0,2}(:?\d{1,2})?$"#
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        let input = ctx.props().input_ref.cast::<HtmlInputElement>().unwrap();
+
+        if let Some(selection) = self.selection {
+            selection.select(&input);
+        }
     }
 }
